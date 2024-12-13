cfd5a9621ced277787f6a944e7ca1577ac8b6ccb
PoeticBear <sihang8973@gmail.com>
2024-12-13 16:35:02 +0800

    change host

--- src/app.tsx
+++ src/app.tsx
@@ -149,6 +149,6 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
 export const request: RequestConfig = {
   // baseURL: 'https://proapi.azurewebsites.net',
   // baseURL: 'http://localhost:3000',
-  baseURL:'https://rqzdsdfzhtjo.sealosbja.site',
+  baseURL:'https://iwxclkclomys.sealosbja.site',
   ...errorConfig,
 };
